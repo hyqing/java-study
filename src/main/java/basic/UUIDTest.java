afcf8a1fad7f110ddf20bb811e90470b15dbd384
hyq07508 <hyq07508@ly.com>
2021-02-19 14:46:58 +0800

    UUID的使用

---
+++ src/main/java/basic/UUIDTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.util.UUID;
+
+/**
+ * Created by hyq on 2021/2/19 14:38.
+ */
+public class UUIDTest {
+    public static void main(String[] args) {
+        byte[] bytes = {2, 3};
+        System.out.println(UUID.fromString("a-b-e-e-f"));
+        System.out.println(UUID.nameUUIDFromBytes(bytes));
+        System.out.println(UUID.randomUUID());
+    }
+}
