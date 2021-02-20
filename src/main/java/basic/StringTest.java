88d693821a3675715c827ec896a9ebafcb4ebde6
hyq07508 <hyq07508@ly.com>
2021-02-20 10:13:10 +0800

    String的使用

---
+++ src/main/java/basic/StringTest.java
@@ -0,0 +1,12 @@
+package basic;
+
+/**
+ * Created by hyq on 2021/2/20 9:45.
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String msg = "Connection reset by peer\\norg.apache.catalina.connector.ClientAbortException: java.io.IOException: Connection reset by peer";
+
+        System.out.println("ww");
+    }
+}
