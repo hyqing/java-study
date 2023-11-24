74d6fd03f72392835f505c59243ee15fbc4d0bcd
hyq07508 <hyq07508@ly.com>
2023-11-24 18:15:34 +0800

    测试

--- src/main/java/basic/UUIDTest.java
+++ src/main/java/basic/UUIDTest.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 /**
  * Created by hyq on 2021/2/19 14:38.
  */
+
+//测试UUID
 public class UUIDTest {
     public static void main(String[] args) {
         byte[] bytes = {2, 3};
